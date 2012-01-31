7e91918c214d740dc4330dcbc30cd090f1848bce
sundeep <sundeep@yedida.net>
2012-01-31 12:48:04 -0800

    Initial Commit

---
+++ pliny.py
@@ -0,0 +1,18 @@
+# Function: set of prototype functions for tracking 
+# Documentation at <insert wiki here>
+
+
+# 'check email for tracking'
+
+def tracking_flow_entry():
+	pass
+
+def check_email_for_tracking(email_text):
+	print "email text:", email_text
+
+def is_tracking_phrase():
+	pass
+	
+# 'setup tracking flow'
+def setup_tracking_flow():
+	pass
---
+++ pliny_play.py
@@ -0,0 +1,10 @@
+# Function: fuck around with pliny
+
+import pliny
+
+def main():
+	# get email text 
+
+	# try shit out
+
+	# 
